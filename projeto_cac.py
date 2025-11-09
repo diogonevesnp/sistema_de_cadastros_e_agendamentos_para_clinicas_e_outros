20feaff39484fa1ffd13781ec7807c599437148d
diogonevesnp <diogonevesnp@gmail.com>
2025-11-09 15:25:19 -0300

    remoção de alguns comentários no código

--- projeto_cac.py
+++ projeto_cac.py
@@ -1,31 +1,3 @@
-'''
-1º PROJETO PESSOAL
-
-[CAC] < Cadastro e Agendamento para Clínicas >
-
-Este sistema tem como objetivo realizar o cadastro de pacientes em clínicas de diferentes segmentos, podendo também ser adaptado
-para outros contextos, como petshops, com pequenos ajustes.
-
-O sistema contará com um menu de opções para manipulação de dados, permitindo:
-- Criar novos cadastros e agendamentos
-- Listar pacientes cadastrados
-- Editar informações
-- Alterar o status de atendimentos
-- Excluir registros
-
-Os cadastros incluem informações como: nome completo, data de nascimento, endereço, DDD, número de telefone, estado, cidade e CPF.  
-Todas as operações de manipulação de dados — cadastro, agendamento, listagem, edição e alteração de status — são filtradas pelo CPF
-do paciente, garantindo maior precisão e segurança.
-
-Além disso, o sistema aplica validações para evitar entradas inválidas, como impedir números em campos de texto e padronizar formatos. Exemplos:
-- CPF sempre com 11 dígitos
-- DDD sempre 2 dígitos
-- Número de telefone formatado com o '9' e  “-” para separação
-
-Este projeto representa a versão inicial da ideia, que poderá evoluir com melhorias futuras, como persistência dos dados em JSON, uso de dicionários
-independentes para cada paciente e implementação de novas funcionalidades.
-'''
-
 agendados = {}
 agendamentos = []
 print('<<< AGENDAMENTO DE CONSULTAS >>>')
